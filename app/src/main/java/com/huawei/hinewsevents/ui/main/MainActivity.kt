fc4e6e4293467cf4891cfbd077f0b7b6baf9b31f
m84161586 <mustafa.can@huawei.com>
2020-07-17 16:02:12 +0300

    push service initial commit.

--- app/src/main/java/com/huawei/hinewsevents/ui/main/MainActivity.kt
+++ app/src/main/java/com/huawei/hinewsevents/ui/main/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.huawei.hinewsevents.ui.main
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.huawei.hinewsevents.R
+import com.huawei.hinewsevents.ui.push.PushService
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getToken()
     }
+
+    private fun getToken(){
+        val context: Context = applicationContext
+        PushService().getToken(context)
+    }
+
+
 
     //We should add Bottom Navigation Bar Menu structure into this activity.
 }
---
+++ app/src/main/java/com/huawei/hinewsevents/ui/push/PushService.kt
@@ -0,0 +1,46 @@
+package com.huawei.hinewsevents.ui.push
+
+import android.content.Context
+import android.text.TextUtils
+import android.util.Log
+import com.huawei.agconnect.config.AGConnectServicesConfig
+import com.huawei.hms.aaid.HmsInstanceId
+import com.huawei.hms.push.HmsMessageService
+import java.lang.Exception
+
+class PushService : HmsMessageService(){
+
+    private val TAG : String = "[PushService]"
+
+    override fun onNewToken(p0: String?) {
+        super.onNewToken(p0)
+        Log.i(TAG, "Receive token $p0")
+        if (p0 != null) {
+            onTokenChanged(p0)
+        }
+    }
+
+    fun getToken(context: Context) {
+        Thread() {
+            kotlin.run {
+                try {
+                    val appId: String = AGConnectServicesConfig.fromContext(context).getString("client/app_id")
+                    val pushtoken: String = HmsInstanceId.getInstance(context).getToken(appId, "HCM")
+
+                    if (!TextUtils.isEmpty(pushtoken)){
+                        Log.i(TAG, "get token $pushtoken")
+                        onTokenChanged(pushtoken)
+                    }else {
+                        Log.i(TAG, "get token is null pushtoken = $pushtoken")
+                    }
+                }catch (e: Exception){
+                    Log.i(TAG, "getToken failed $e")
+                }
+            }
+        }.start()
+    }
+
+    fun onTokenChanged(token: String) {
+        Log.i(TAG, "Token is: $token")
+    }
+}
