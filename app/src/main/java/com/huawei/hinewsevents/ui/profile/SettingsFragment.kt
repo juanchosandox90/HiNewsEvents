bf00720d8a694014ceb8d4a7bb1018ee0920ee12
Baris Can Kurtulmus 00264678 <baris.can.kurtulmus@huawei.com>
2020-08-07 20:37:00 +0300

    TaskName : [HiNews&Events App][DTSE-2932][Settings Page UI&UX Integration and Test]
    - Settings uı page look and feel fixes
    - Icons and right arrows added to settingsUI

--- app/src/main/java/com/huawei/hinewsevents/ui/profile/SettingsFragment.kt
+++ app/src/main/java/com/huawei/hinewsevents/ui/profile/SettingsFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.huawei.agconnect.auth.AGConnectAuth
@@ -23,49 +24,85 @@ class SettingsFragment: Fragment() {
         val view = inflater.inflate(R.layout.fragment_user_settings, container, false)
 
         view.findViewById<TextView>(R.id.item_font_size).setOnClickListener {
-            Utils.showToastMessage(context, "Font Size Change Request!");
+            Utils.showToastMessage(context, "Font Size Change Request!")
         }
         view.findViewById<TextView>(R.id.item_font_size_value).setOnClickListener {
-            Utils.showToastMessage(context, "Font Size Change Request!");
+            Utils.showToastMessage(context, "Font Size Change Request!")
+        }
+        view.findViewById<ImageView>(R.id.goDetailIcon1).setOnClickListener {
+            Utils.showToastMessage(context, "Font Size Change Request!")
         }
         view.findViewById<TextView>(R.id.item_language).setOnClickListener {
-            Utils.showToastMessage(context, "Language Change Request!");
+            Utils.showToastMessage(context, "Language Change Request!")
         }
         view.findViewById<TextView>(R.id.item_language_value).setOnClickListener {
-            Utils.showToastMessage(context, "Language Change Request!");
+            Utils.showToastMessage(context, "Language Change Request!")
         }
-        view.findViewById<TextView>(R.id.item_kullanim).setOnClickListener {
-            Utils.showToastMessage(context, "Kullanım Şekli Change Request!");
+        view.findViewById<ImageView>(R.id.goDetailIcon2).setOnClickListener {
+            Utils.showToastMessage(context, "Language Change Request!")
         }
-        view.findViewById<TextView>(R.id.item_kullanim_value).setOnClickListener {
-            Utils.showToastMessage(context, "Kullanım Şekli Change Request!");
+        view.findViewById<TextView>(R.id.item_appUsage).setOnClickListener {
+            Utils.showToastMessage(context, "App Usage Change Request!")
+        }
+        view.findViewById<TextView>(R.id.item_appUsage_value).setOnClickListener {
+            Utils.showToastMessage(context, "App Usage Change Request!")
+        }
+        view.findViewById<ImageView>(R.id.goDetailIcon3).setOnClickListener {
+            Utils.showToastMessage(context, "App Usage Change Request!")
         }
         view.findViewById<TextView>(R.id.item_feedback).setOnClickListener {
-            Utils.showToastMessage(context, "Leave Feeback  Request!");
+            Utils.showToastMessage(context, "Leave Feeback  Request!")
+        }
+        view.findViewById<ImageView>(R.id.goDetailIcon4).setOnClickListener {
+            Utils.showToastMessage(context, "Leave Feeback  Request!")
         }
         view.findViewById<TextView>(R.id.item_clearCache).setOnClickListener {
-            Utils.showToastMessage(context, "Clear Cash Request!");
+            Utils.showToastMessage(context, "Clear Cash Request!")
         }
         view.findViewById<TextView>(R.id.item_clearCache_value).setOnClickListener {
-            Utils.showToastMessage(context, "Clear Cash Request!");
+            Utils.showToastMessage(context, "Clear Cash Request!")
+        }
+        view.findViewById<ImageView>(R.id.goDetailIcon5).setOnClickListener {
+            Utils.showToastMessage(context, "Clear Cash Request!")
         }
         view.findViewById<TextView>(R.id.item_inviteFriends).setOnClickListener {
-            Utils.showToastMessage(context, "Invite Friends Request!");
+            Utils.showToastMessage(context, "Invite Friends Request!")
+        }
+        view.findViewById<TextView>(R.id.item_inviteFriends).setOnClickListener {
+            Utils.showToastMessage(context, "Invite Friends Request!")
+        }
+        view.findViewById<ImageView>(R.id.goDetailIcon6).setOnClickListener {
+            Utils.showToastMessage(context, "Invite Friends Request!")
         }
         view.findViewById<TextView>(R.id.item_privacyStatement).setOnClickListener {
-            Utils.showToastMessage(context, "Privacy Statement Request!");
+            Utils.showToastMessage(context, "Privacy Statement Request!")
+        }
+        view.findViewById<ImageView>(R.id.goDetailIcon7).setOnClickListener {
+            Utils.showToastMessage(context, "Privacy Statement Request!")
         }
         view.findViewById<TextView>(R.id.item_terms).setOnClickListener {
-            Utils.showToastMessage(context, "Terms of Service Request!");
+            Utils.showToastMessage(context, "Terms of Service Request!")
+        }
+        view.findViewById<ImageView>(R.id.goDetailIcon8).setOnClickListener {
+            Utils.showToastMessage(context, "Terms of Service Request!")
         }
         view.findViewById<TextView>(R.id.item_licenses).setOnClickListener {
-            Utils.showToastMessage(context, "Third Party Licenses Request!");
+            Utils.showToastMessage(context, "Third Party Licenses Request!")
+        }
+        view.findViewById<ImageView>(R.id.goDetailIcon9).setOnClickListener {
+            Utils.showToastMessage(context, "Third Party Licenses Request!")
         }
         view.findViewById<TextView>(R.id.item_aboutUs).setOnClickListener {
-            Utils.showToastMessage(context, "About Us Request!");
+            Utils.showToastMessage(context, "About Us Request!")
+        }
+        view.findViewById<ImageView>(R.id.goDetailIcon10).setOnClickListener {
+            Utils.showToastMessage(context, "About Us Request!")
         }
         view.findViewById<TextView>(R.id.item_rateUs).setOnClickListener {
-            Utils.showToastMessage(context, "Rate Us Request!");
+            Utils.showToastMessage(context, "Rate Us Request!")
+        }
+        view.findViewById<ImageView>(R.id.goDetailIcon11).setOnClickListener {
+            Utils.showToastMessage(context, "Rate Us Request!")
         }
         view.findViewById<TextView>(R.id.logOut_button).setOnClickListener {
             signOut()
@@ -74,10 +111,10 @@ class SettingsFragment: Fragment() {
     }
     private fun signOut() {
         AGConnectAuth.getInstance().signOut()
-        Utils.showToastMessage(context, "SignOut Success");
+        Utils.showToastMessage(context, "SignOut Success")
         Log.i(TAG,"signOut Success")
         //Utils.loadAndSetImageWithGlide( context, profileImage, failedImageUri );
-        loggedIn = false;
+        loggedIn = false
         //loginLogOut.text = "Login with HuaweiID"
         //nameSurname.text = "Name Surname"
     }
