69be051c93fb77f8a3fa1ba89728ede78d8c4671
m84161586 <mustafa.can@huawei.com>
2020-08-07 16:46:55 +0300

    Library section of profile changed. New Bookmarks (visible) and Downloads (gone) added. Their drawables (icons), and bookmarks click activity added too.

---
+++ app/src/main/java/com/huawei/hinewsevents/ui/profile/BookmarksActivity.kt
@@ -0,0 +1,12 @@
+package com.huawei.hinewsevents.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.huawei.hinewsevents.R
+
+class BookmarksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bookmarks)
+    }
+}
