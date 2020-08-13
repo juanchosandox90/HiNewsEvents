2e7081e681f22d00bc0b2981a2afc57661fc6959
Ubeyde Akgul <ubeyde.akgul@huawei.com>
2020-08-13 13:11:47 +0300

    [Description] :some fixes on ui.

--- app/src/main/java/com/huawei/hinewsevents/ui/profile/BookmarksActivity.kt
+++ app/src/main/java/com/huawei/hinewsevents/ui/profile/BookmarksActivity.kt
@@ -1,18 +1,33 @@
 package com.huawei.hinewsevents.ui.profile
 
-import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.app.NavUtils
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.huawei.hinewsevents.R
 
 class BookmarksActivity : AppCompatActivity() {
+
+    val TAG: String = BookmarksActivity::class.simpleName.toString()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bookmarks)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.title = "Profile Bookmarks"
 
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        Log.d(TAG, "onBackPressed")
+        finish()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        Log.d(TAG, "onSupportNavigateUp")
+        onBackPressed()
+        return true
     }
 }
